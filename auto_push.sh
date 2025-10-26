b00aeadde69d9588f7d1786bf94ab70aeef25bc6
Arya Sumant <studywithpwno.1@gmail.com>
2025-10-26 21:25:44 +0530

    auto update 2025-10-26 21:25:44

--- auto_push.sh
+++ auto_push.sh
@@ -4,5 +4,5 @@ do
 git add .
 git commit -m "auto update $(date '+%Y-%m-%d %H:%M:%S')"
 git push origin main
-sleep 60 # har 1 minute baad push karega
+sleep 30 # har 1 minute baad push karega
 done
